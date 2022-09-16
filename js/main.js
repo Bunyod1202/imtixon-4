7619b44ed128f64ed775c84695f0fc058bf3660f
Bunyood <bunyodbunyod035@gmail.com>
2022-09-16 15:26:24 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+  margin: 32,
+  loop: true,
+  center: false,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+      margin: 20,
+    },
+    600: {
+      items: 1.5,
+      nav: true,
+      margin: 0,
+    },
+    1000: {
+      items: 3.5,
+      nav:true,
+    }
+  }
+})
+
